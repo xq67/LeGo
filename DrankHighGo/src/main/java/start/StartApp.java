cd0dc292a409e6b3b9783e3b5947cce7574a2d2c
nanchunxiao <38347314+nanchunxiao@users.noreply.github.com>
2018-04-21 10:33:10 +0800

    修改支持jsp

--- DrankHighGo/src/main/java/start/StartApp.java
+++ DrankHighGo/src/main/java/start/StartApp.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 
@@ -16,7 +18,11 @@ import com.jinglun.util.InitListener;
 @ComponentScan("com.jinglun.controller,com.jinglun.biz,com.jinglun.util")//扫描biz，controller组件
 @MapperScan("com.jinglun.dao")    //扫描mapper组件
 @EnableAutoConfiguration
-public class StartApp {
+public class StartApp extends SpringBootServletInitializer {
+	@Override  
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {  
+        return application.sources(StartApp.class);  
+    }
 public static void main(String[] args) {
 	SpringApplication springApplication = new SpringApplication(StartApp.class);
 	springApplication.addListeners(new InitListener());
